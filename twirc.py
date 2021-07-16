b5baa90b2f7cf1bb0814f390eebdaa58e4ce815a
Rick Vause <rvause@gmail.com>
2021-07-16 13:45:41 +0200

    Checkpoint because I need to write tests

--- twirc.py
+++ twirc.py
@@ -1,12 +1,17 @@
 import re
 import socket
 import time
+from collections import namedtuple
 
 from conf import settings
 
 
-PING_RE = re.compile(r"^PING\s:(\w+\.twitch\.tv)")
-PARSER_RE = re.compile(r"^:\w+!\w+@\w+\.\w+\.twitch\.tv\s(?P<type>\w+)\s(?P<channel>[\#\w]+)\s:(?P<message>.*)(?:\n|$)")
+PING_RE = re.compile("^PING\s:(\w+\.twitch\.tv)")
+# 1: Username
+# 2: Action/Command
+# 3: Channel
+METADATA_PARSER = re.compile("^([0-9a-zA-Z\_]+)![0-9a-zA-Z\_]+@[0-9a-zA-Z\_]+\.[0-9a-zA-Z]+\.twitch\.tv\ ([0-9a-zA-Z\_]+)\ ([\#0-9a-zA-Z_]+)$")
+TAG_PARSER = re.compile("maybe")
 
 
 def to_bytes(val):
@@ -17,9 +22,69 @@ def from_bytes(val):
     return val.decode()
 
 
-class IRCClient:
+BaseMessage = namedtuple("Message", ["tags_str", "username", "action", "channel", "content"])
+
+
+class Message(BaseMessage):
+    @classmethod
+    def from_text(cls, data):
+        """
+        Given a Twitch IRC message text including tags, parse
+        the message and return a BaseMessage tuple.
+
+        Each message has three disctinct parts:
+            1. tags https://dev.twitch.tv/docs/irc/tags
+            2. metadata - username, action/command, channel
+            3. content - the actual content of the message such as the text someone entered
+
+        """
+        try:
+            tags_str, metadata_str, content = [p.strip() for p in data.split(":")]
+        except ValueError:
+            return None
+
+
+        metadata = METADATA_PARSER.match(metadata_str)
+
+        if metadata is not None:
+            return cls(
+                tags_str,
+                metadata.group(1),
+                metadata.group(2),
+                metadata.group(3),
+                content,
+            )
+        return None
+
+    def _parse_tags(self):
+        """
+        Lazy parsing for tags.
+
+        """
+        tokens = self.tags_str.split(";")
+        self._tags = {
+            k: v
+            for token in tokens
+            for k, v in [token.split("=")]
+        }
+
+    @property
+    def tags(self):
+        """
+        Tags exist as a string on the object until this property is
+        accessed when the tags will be parsed.
+
+        This will return a dictionary of tags for the message.
+
+        """
+        if not hasattr(self, "_tags"):
+            self._parse_tags()
+        return self._tags
+
+
+class Client:
     """
-    Handles a connection to a Twitch IRC channel
+    Handles a connection to a Twitch IRC channel as well as callbacks for events.
 
     """
     def __init__(self, password, nick, channel, addr=None):
@@ -29,17 +94,31 @@ class IRCClient:
         self.channel = "#{}".format(channel) if not channel.startswith("#") else channel
         self.sock = None
         self.is_connected = False
+        self._registry = dict()
 
     def connect(self):
+        """
+        Connects to the IRC server and joins the given channel.
+        This also will request tags in messages.
+
+        """
         self.sock = socket.socket()
         self.sock.connect(self.addr)
         self.send("PASS {}".format(self.password))
         self.send("NICK {}".format(self.nick))
         self.send("JOIN {}".format(self.channel))
+        self.send("CAP REQ :twitch.tv/tags")
         self.is_connected = True
         # TODO: error handling
 
-    def listen(self):
+    def _listen(self):
+        """
+        Main listen loop.
+
+        First ensures the client is connected. Following that it will
+        receive messages every second and call the handler function.
+
+        """
         if not self.is_connected:
             self.connect()
 
@@ -48,19 +127,79 @@ class IRCClient:
             ping = PING_RE.match(data)
             if ping:
                 self.handle_ping(ping.group(1))
-            print(data)
+            else:
+                result = self.handle_message(data)
+
+            if result:
+                print(result)
+
             time.sleep(1)
 
+    def listen(self):
+        """
+        Wrapper function to cleanly handle interrupts from the
+        listen loop.
+
+        """
+        try:
+            self._listen()
+        except KeyboardInterrupt:
+            self.sock.close()
+            self.is_connected = False
+
     def recv(self):
+        """
+        Receive messages from the server and convert to string.
+
+        """
         return from_bytes(self.sock.recv(512))
 
     def send(self, val, end="\n"):
+        """
+        Send message to the server, handling line ending and conversion to bytestring.
+
+        """
         self.sock.send(to_bytes("{val}{end}".format(val=val, end=end)))
 
     def handle_ping(self, host):
-        self.send("PING :{}".format(host))
+        """
+        Called when Twitch IRC sends a PING message and responds with PONG.
+
+        See: https://dev.twitch.tv/docs/irc/guide#connecting-to-twitch-irc
+
+        """
+        self.send("PONG :{}".format(host))
+
+    def handle_message(self, data):
+        """
+        Currently not final. Debugs message parsing.
+
+        """
+        message = Message.from_text(data)
+        if message is not None:
+            print(message.username, message.action, message.channel, message.content)
+            for tag_name, value in message.tags.items():
+                print(tag_name, ":", value)
+        print(data)
+
+    def regiter_handler(self, event, fn, unique=False):
+        """
+        Accepts an event and function and registers that function as
+        an event handler to be called.
+
+        If unique is set to true then all other handlers will be removed.
+
+        """
+        if event not in self._registry or unique:
+            self._registry[event] = [fn]
+        else:
+            self._registry[event].append(fn)
+        return fn
+
+    def on_message(self, fn, unique=False):
+        return self.regiter_handler("message", fn, unique=unique)
 
 
 # Client for testing things, remove me later
-client = IRCClient(settings.TWITCH_IRC_TOKEN, settings.TWITCH_NICK, settings.TWITCH_CHANNEL)
+client = Client(settings.TWITCH_IRC_TOKEN, settings.TWITCH_NICK, settings.TWITCH_CHANNEL)
 #client.connect()
